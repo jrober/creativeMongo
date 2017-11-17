dcefde28abafeef7b8f641053ce89aa2bc556ffc
Thecooljason <31867236+Thecooljason@users.noreply.github.com>
2017-11-16 23:16:09 -0700

    New version of the website

---
+++ Creative Project 5/public/javascripts/frontapp.js
@@ -0,0 +1,49 @@
+angular.module('comment', [])
+.controller('MainCtrl', [
+  '$scope','$http',
+  function($scope,$http){
+    $scope.comments = [];
+    $scope.addComment = function() {
+      var newcomment = {title:$scope.formContent,upvotes:0, name:$scope.name,
+		address:$scope.address, phoneNumber:$scope.phoneNumber, email:$scope.email
+	  };
+      $scope.formContent='';
+      $http.post('/comments', newcomment).success(function(data){
+        $scope.comments.push(data);
+      });
+    };
+    $scope.upvote = function(comment) {
+      return $http.put('/comments/' + comment._id + '/upvote')
+        .success(function(data){
+          console.log("upvote worked");
+          comment.upvotes = data.upvotes;
+        });
+    };
+	$scope.incrementUpvotes = function(comment) {
+	  $scope.upvote(comment);
+    };
+    $scope.getAll = function() {
+      return $http.get('/comments').success(function(data){
+        angular.copy(data, $scope.comments);
+      });
+    };
+    $scope.getAll();
+
+	$scope.delete = function(comment) {
+      $http.delete('/comments/' + comment._id )
+        .success(function(data){
+          console.log("delete worked");
+        });
+      $scope.getAll();
+    };
+	
+	$scope.deleteAll = function(comment) {
+      $http.delete('/comments/')
+        .success(function(data){
+          console.log("delete worked");
+        });
+      $scope.getAll();
+    };
+	
+  }
+]);
